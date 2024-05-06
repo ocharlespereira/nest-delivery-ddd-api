7c187f4580867ca511d7bd5c581ba262f95aa9eb
CharlesPereira <charlesti.ptu@gmail.com>
2024-05-06 10:55:01 -0300

    chore: create path enterprise and application

---
+++ src/domain/delivery/application/repositories/deliveryman-repository.ts
@@ -0,0 +1,5 @@
+import { Deliveryman } from '@domain/delivery/enterprise/entities/deliveryman'
+
+export abstract class DeliverymanRepository {
+  abstract create(deliveryman: Deliveryman): Promise<void>
+}
