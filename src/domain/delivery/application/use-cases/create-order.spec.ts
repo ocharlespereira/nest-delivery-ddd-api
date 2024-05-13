e5dea3c63f0229f4507276e4551a3083a2b2d7fa
CharlesPereira <charlesti.ptu@gmail.com>
2024-05-13 09:49:11 -0300

    test: create a new order

---
+++ src/domain/delivery/application/use-cases/create-order.spec.ts
@@ -0,0 +1,25 @@
+import { CreateOrderUserCase } from './create-order'
+import { InMemoryOrderRepository } from 'test/repositories/in-memory-order-repository'
+
+let inMemoryOrderRepository: InMemoryOrderRepository
+let sut: CreateOrderUserCase
+
+describe('Create Order', () => {
+  beforeEach(() => {
+    inMemoryOrderRepository = new InMemoryOrderRepository()
+
+    sut = new CreateOrderUserCase(inMemoryOrderRepository)
+  })
+
+  it('should create a order', async () => {
+    console.log('sut :', sut)
+    const { order } = await sut.execute({
+      deliverymanId: '1',
+      recipientId: '2',
+      product: 'Product',
+      status: 'Pending',
+    })
+
+    expect(order.id).toBeTruthy()
+  })
+})
