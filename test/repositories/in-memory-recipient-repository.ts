db7ad118043c7187e0a7e9d478054cec65bd285a
CharlesPereira <charlesti.ptu@gmail.com>
2024-05-14 18:36:12 -0300

    feat: create inmemory repicient

---
+++ test/repositories/in-memory-recipient-repository.ts
@@ -0,0 +1,10 @@
+import { RecipientRepository } from '@/domain/delivery/application/repositories/recipient-repository'
+import { Recipient } from '@/domain/delivery/enterprise/entities/recipient'
+
+export class InMemoryRecipientRepository implements RecipientRepository {
+  public items: Recipient[] = []
+
+  async create(recipient: Recipient) {
+    this.items.push(recipient)
+  }
+}
