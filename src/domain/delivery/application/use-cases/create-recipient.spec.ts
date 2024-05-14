c21018c1d45663669fe206cf554595079832e693
CharlesPereira <charlesti.ptu@gmail.com>
2024-05-14 18:36:20 -0300

    test: create recipient use case

---
+++ src/domain/delivery/application/use-cases/create-recipient.spec.ts
@@ -0,0 +1,24 @@
+import { CreateRecipientUseCase } from './create-recipient'
+import { InMemoryRecipientRepository } from 'test/repositories/in-memory-recipient-repository'
+
+let inMemoryRecipientRepository: InMemoryRecipientRepository
+let sut: CreateRecipientUseCase
+
+describe('Create Recipient', () => {
+  beforeEach(() => {
+    inMemoryRecipientRepository = new InMemoryRecipientRepository()
+
+    sut = new CreateRecipientUseCase(inMemoryRecipientRepository)
+  })
+
+  it('should create a recipient', async () => {
+    console.log('sut :', sut)
+    const { recipient } = await sut.execute({
+      address: 'Address',
+      name: 'Name',
+      phoneNumber: 'Phone Number',
+    })
+
+    expect(recipient.id).toBeTruthy()
+  })
+})
