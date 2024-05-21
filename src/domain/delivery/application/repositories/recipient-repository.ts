c02905a1a6c895ff7b0a3a99ce1819d8369c2cf2
CharlesPereira <charlesti.ptu@gmail.com>
2024-05-21 18:05:20 -0300

    feat: delete recipient usecase, repository and inmemory

--- src/domain/delivery/application/repositories/recipient-repository.ts
+++ src/domain/delivery/application/repositories/recipient-repository.ts
@@ -1,5 +1,7 @@
 import { Recipient } from '../../enterprise/entities/recipient'
 
 export abstract class RecipientRepository {
+  abstract findById(id: string): Promise<Recipient | null>
   abstract create(recipientman: Recipient): Promise<void>
+  abstract delete(recipient: Recipient): Promise<void>
 }
--- test/repositories/in-memory-recipient-repository.ts
+++ test/repositories/in-memory-recipient-repository.ts
@@ -4,7 +4,21 @@ import { Recipient } from '@/domain/delivery/enterprise/entities/recipient'
 export class InMemoryRecipientRepository implements RecipientRepository {
   public items: Recipient[] = []
 
+  async findById(id: string) {
+    const recipient = this.items.find((item) => item.id.toString() === id)
+
+    if (!recipient) {
+      return null
+    }
+
+    return recipient
+  }
+
   async create(recipient: Recipient) {
     this.items.push(recipient)
+  }
+
+  async delete(recipient: Recipient) {
+    this.items = this.items.filter((item) => item.id !== recipient.id)
   }
 }
