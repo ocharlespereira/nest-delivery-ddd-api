bf15c9c8acfb99a91a5eb94c3aaf353976e8bb10
CharlesPereira <charlesti.ptu@gmail.com>
2024-05-22 19:09:55 -0300

    test: delete deliveryman

---
+++ src/domain/delivery/application/use-cases/delete-deliveryman.spec.ts
@@ -0,0 +1,30 @@
+import { makeDeliveryman } from 'test/factories/make-deliveryman'
+import { DeleteDeliverymanUserCase } from './delete-deliveryman'
+import { InMemoryDeliverymanRepository } from 'test/repositories/in-memory-deliveryman-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryDeliverymanRepository: InMemoryDeliverymanRepository
+let sut: DeleteDeliverymanUserCase
+
+describe('Delete Deliveryman', () => {
+  beforeEach(() => {
+    inMemoryDeliverymanRepository = new InMemoryDeliverymanRepository()
+
+    sut = new DeleteDeliverymanUserCase(inMemoryDeliverymanRepository)
+  })
+
+  it('should delete a deliveryman', async () => {
+    const newDeliveryman = makeDeliveryman(
+      {},
+      new UniqueEntityID('deliveryman-1'),
+    )
+
+    await inMemoryDeliverymanRepository.create(newDeliveryman)
+
+    await sut.execute({
+      deliverymanId: 'deliveryman-1',
+    })
+
+    expect(inMemoryDeliverymanRepository.items.length).toBe(0)
+  })
+})
