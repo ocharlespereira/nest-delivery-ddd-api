dc750fd820202a84d9fd3bf74ced982e728d08ce
CharlesPereira <charlesti.ptu@gmail.com>
2024-06-04 20:34:35 -0300

    test: refacture create order add correlation for deliveryman and recipient

--- src/domain/delivery/application/use-cases/create-order.spec.ts
+++ src/domain/delivery/application/use-cases/create-order.spec.ts
@@ -1,23 +1,47 @@
+import { InMemoryDeliverymanRepository } from 'test/repositories/in-memory-deliveryman-repository'
 import { CreateOrderUserCase } from './create-order'
 import { InMemoryOrderRepository } from 'test/repositories/in-memory-order-repository'
+import { InMemoryRecipientRepository } from 'test/repositories/in-memory-recipient-repository'
+import { makeDeliveryman } from 'test/factories/make-deliveryman'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeRecipient } from 'test/factories/make-recipient'
 
 let inMemoryOrderRepository: InMemoryOrderRepository
+let inMemoryDeliverymanRepository: InMemoryDeliverymanRepository
+let inMemoryRecipientRepository: InMemoryRecipientRepository
 let sut: CreateOrderUserCase
 
 describe('Create Order', () => {
   beforeEach(() => {
     inMemoryOrderRepository = new InMemoryOrderRepository()
+    inMemoryDeliverymanRepository = new InMemoryDeliverymanRepository()
+    inMemoryRecipientRepository = new InMemoryRecipientRepository()
 
-    sut = new CreateOrderUserCase(inMemoryOrderRepository)
+    sut = new CreateOrderUserCase(
+      inMemoryOrderRepository,
+      inMemoryDeliverymanRepository,
+      inMemoryRecipientRepository,
+    )
   })
 
   it('should create a order', async () => {
+    const newDeliveryman = makeDeliveryman({
+      idUser: new UniqueEntityID('deliveryman-1'),
+    })
+
+    await inMemoryDeliverymanRepository.create(newDeliveryman)
+
+    const newRecipient = makeRecipient({
+      recipientId: new UniqueEntityID('recipient-1'),
+    })
+
+    await inMemoryRecipientRepository.create(newRecipient)
+
     const { order } = await sut.execute({
-      orderId: '1',
-      deliverymanId: '1',
-      recipientId: '2',
+      deliverymanId: newDeliveryman.id.toString(),
+      recipientId: newRecipient.id.toString(),
       product: 'Product',
-      status: 'Pending',
+      status: 'waiting',
     })
 
     expect(order.id).toBeTruthy()
