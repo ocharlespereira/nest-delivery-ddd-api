194429bc9db897785cc82fc393d0af67be1ac355
CharlesPereira <charlesti.ptu@gmail.com>
2024-06-18 10:16:01 -0300

    chore: add type boolean for success and error

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,19 +1,35 @@
-export class Success<T> {
+export class Success<T, E> {
   readonly value: T
 
   constructor(value: T) {
     this.value = value
   }
+
+  isSuccess(): this is Success<T, E> {
+    return true
+  }
+
+  isFailure(): this is Failure<T, E> {
+    return false
+  }
 }
-export class Failure<E> {
+export class Failure<T, E> {
   readonly value: E
 
   constructor(value: E) {
     this.value = value
   }
+
+  isSuccess(): this is Success<T, E> {
+    return false
+  }
+
+  isFailure(): this is Failure<T, E> {
+    return true
+  }
 }
 
-export type Either<T, E> = Success<T> | Failure<E>
+export type Either<T, E> = Success<T, E> | Failure<T, E>
 
 export const success = <T, E>(value: T): Either<T, E> => {
   return new Success(value)
