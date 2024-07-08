a18d8c7c669bd8627f83659c477872f90c697ce2
CharlesPereira <charlesti.ptu@gmail.com>
2024-07-08 19:20:43 -0300

    chore: add resource not found error with parameters

--- src/domain/delivery/application/use-cases/get-recipient-by-name.ts
+++ src/domain/delivery/application/use-cases/get-recipient-by-name.ts
@@ -1,13 +1,18 @@
+import { Either, failure, success } from '@/core/either'
 import { Recipient } from '../../enterprise/entities/recipient'
 import { RecipientRepository } from '../repositories/recipient-repository'
+import { ResourceNotFoundError } from './errors/resources-not-found-error'
 
 interface GetRecipientByNameUserCaseRequest {
   name: string
 }
 
-interface GetRecipientByNameUserCaseResponse {
-  recipient: Recipient
-}
+type GetRecipientByNameUserCaseResponse = Either<
+  {
+    recipient: Recipient
+  },
+  ResourceNotFoundError
+>
 
 export class GetRecipientByNameUserCase {
   constructor(private recipientRepository: RecipientRepository) {}
@@ -18,11 +23,11 @@ export class GetRecipientByNameUserCase {
     const recipient = await this.recipientRepository.findByName(name)
 
     if (!recipient) {
-      throw new Error('Recipient not found')
+      return failure(new ResourceNotFoundError('Recipient not found'))
     }
 
-    return {
+    return success({
       recipient,
-    }
+    })
   }
 }
