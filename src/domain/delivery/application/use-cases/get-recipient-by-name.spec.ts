88702c1540b041da3864b9b7140c39634742ecc2
CharlesPereira <charlesti.ptu@gmail.com>
2024-07-08 19:20:48 -0300

    test: create and add resource not found error with parameters

--- src/domain/delivery/application/use-cases/get-recipient-by-name.spec.ts
+++ src/domain/delivery/application/use-cases/get-recipient-by-name.spec.ts
@@ -1,6 +1,7 @@
 import { makeRecipient } from 'test/factories/make-recipient'
 import { GetRecipientByNameUserCase } from './get-recipient-by-name'
 import { InMemoryRecipientRepository } from 'test/repositories/in-memory-recipient-repository'
+import { ResourceNotFoundError } from './errors/resources-not-found-error'
 
 let inMemoryRecipientRepository: InMemoryRecipientRepository
 let sut: GetRecipientByNameUserCase
@@ -17,18 +18,19 @@ describe('Get Recipient', () => {
 
     await inMemoryRecipientRepository.create(newRecipient)
 
-    const { recipient } = await sut.execute({
+    const result = await sut.execute({
       name: 'new name',
     })
 
-    expect(recipient.id).toBeTruthy()
+    expect(result.isSuccess).toBeTruthy()
   })
 
   it('should throw an error if the recipient is not found', async () => {
-    await expect(
-      sut.execute({
-        name: 'nonexistent name',
-      }),
-    ).rejects.toThrow('Recipient not found')
+    const result = await sut.execute({
+      name: 'nonexistent name',
+    })
+
+    expect(result.isFailure).toBeTruthy()
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 })
