a13e384002827989385c08b933817e318c3e2104
CharlesPereira <charlesti.ptu@gmail.com>
2024-07-12 18:04:25 -0300

    chore: add resource not found error with parameters

--- src/domain/delivery/application/use-cases/order-status-waiting.ts
+++ src/domain/delivery/application/use-cases/order-status-waiting.ts
@@ -1,14 +1,19 @@
+import { Either, failure, success } from '@/core/either'
 import { Order } from '../../enterprise/entities/order'
 import { OrderRepository } from '../repositories/order-repository'
+import { StatusError } from './errors/status-error'
 
 interface OrderStatusUseCaseRequest {
   orderId: string
   status: 'waiting' | 'in-transit' | 'delivered' | 'canceled'
 }
 
-interface OrderStatusUseCaseResponse {
-  order: Order
-}
+type OrderStatusUseCaseResponse = Either<
+  {
+    order: Order
+  },
+  StatusError
+>
 
 export class OrderStatusUseCase {
   constructor(private orderRepository: OrderRepository) {}
@@ -25,16 +30,16 @@ export class OrderStatusUseCase {
 
     const validStatuses = ['waiting', 'in-transit', 'delivered', 'canceled']
 
-    if(!validStatuses.includes(status)) {
-      throw new Error('Invalid status')
+    if (!validStatuses.includes(status)) {
+      return failure(new StatusError())
     }
 
     order.status = status
 
     await this.orderRepository.save(order)
 
-    return {
+    return success({
       order,
-    }
+    })
   }
 }
